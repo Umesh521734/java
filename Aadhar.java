5fd2fa628fc945b7362da27da6ac0da35dff2586
Umesh521734 <Umeshchandra521734@gmail.com>
2025-03-02 23:17:33 +0530

    Add files via upload

---
+++ Aadhar.java
@@ -0,0 +1,10 @@
+class Aadhar{
+public static void main(String[] args){
+String proof="Aadhar";
+if(proof.contains("Aadhar")){
+System.out.println("Verified");}
+else{
+System.out.println("Not verified");}
+System.out.println("String checking completed");
+}
+}
---
+++ area_rectangle.java
@@ -0,0 +1,9 @@
+public class area_rectangle {
+    public static void main(String[] args) {
+        int length = 5;
+        int width = 3;
+        int area = length * width;
+        System.out.println("The area of the rectangle is " + area);
+
+    }
+}
